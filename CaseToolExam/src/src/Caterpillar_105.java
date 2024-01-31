c345dc39593ee9fbed7a9891ed1c0454eebf3b38
AdrinaFernandesCaseTools <adrinafernandes16@gmail.com>
2024-01-31 09:18:03 +0530

    commit

---
+++ CaseToolExam/src/src/Caterpillar_105.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Caterpillar_105 {
+
+		int moves,cabbages;
+		Caterpillar_105(int moves,int cabbages)
+		{
+			this.moves=moves;
+			this.cabbages=cabbages;
+		}
+
+	
+
+}
